a0e5e344bad09a867fd9e191bb65dde825630265
bdanzer <britt.danzer@gmail.com>
2020-01-24 14:55:47 -0600

    fixing some ie11 issues

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'react-app-polyfill/ie11';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
